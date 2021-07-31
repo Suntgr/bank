5c7c26fb3db3d684fba2792ede7cd6d3b644a835
suntgr <suntgr@163.com>
2021-07-31 20:31:46 +0800

    22

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,35 +1,97 @@
 import './App.css'
 import logo from '../../assets/images/logo.png'
-// import { useHistory } from 'react-router-dom'
-import { useState, useCallback } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
+import Toast from '../../components/toast'
+import Loading from '../../components/loading'
+
+const COUNTDOWN_SECONDS = 5
 
 function App() {
-  // let history = useHistory()
+  let history = useHistory()
   const [state, setState] = useState({
     number: '',
     phone: '',
-    code: ''
+    code: '',
   })
   const handle = (e, key) => {
     setState({
       ...state,
-      [key]: e.target.value
+      [key]: e.target.value,
     })
   }
-  const getCode = useCallback(() => {
-    console.log(state)
+  const getCode = () => {
+    if (!state.phone) {
+      return Toast.info('请输入手机号码')
+    }
+    if (!/^1[3-9]\d{9}$/.test(state.phone)) {
+      return Toast.info('手机号码有误')
+    }
+    setTiming(true)
     axios({
-      url: '/arabica/coffee/getCodeByPhone',
+      url: 'https://gf.ilinkmore.com/arabica/getCodeByPhone',
       method: 'post',
-      data: { phone: state.phone }
-    }).then(() => {
-      console.log('发送成功')
+      data: { phone: state.phone },
     })
-  }, [state])
-  const submit = useCallback(() => {
-    console.log(state)
-  }, [state])
+      .then(({ data }) => {
+        if (+data.code === 200) {
+          Toast.info('发送成功')
+        } else {
+          Toast.info('发送失败')
+        }
+      })
+      .catch(() => {
+        Toast.info('发送失败')
+      })
+  }
+  const submit = () => {
+    if (state.number.length !== 12) {
+      return Toast.info('兑换券号码有误')
+    } 
+    Loading.show()
+    axios({
+      url: 'https://gf.ilinkmore.com/arabica/updateCouponCode',
+      method: 'post',
+      data: { ticket: state.number, phone: state.phone, code: state.code },
+    })
+      .then(() => {
+        sessionStorage.setItem('code', 22)
+        history.push('/details')
+      })
+      .catch((err) => {
+        console.log(1111, err)
+        Toast.info('兑换失败')
+      })
+      .finally(() => {
+        Loading.hide()
+      })
+  }
+
+  const [timing, setTiming] = useState(false)
+  // 当前秒数
+  const [second, setSecond] = useState(COUNTDOWN_SECONDS)
+
+  // 首次渲染和 timing 变化时触发 effect
+  useEffect(() => {
+    let interval
+    // 开始倒计时
+    if (timing) {
+      interval = setInterval(() => {
+        setSecond((preSecond) => {
+          if (preSecond <= 1) {
+            setTiming(false)
+            clearInterval(interval)
+            // 重置秒数
+            return COUNTDOWN_SECONDS
+          } else {
+            return preSecond - 1
+          }
+        })
+      }, 1000)
+    }
+    return () => clearInterval(interval)
+  }, [timing])
 
   return (
     <div className="app">
@@ -43,7 +105,7 @@ function App() {
             placeholder="请输入12位兑换券号码"
             maxLength="12"
             value={state.number}
-            onChange={e => handle(e, 'number')}
+            onChange={(e) => handle(e, 'number')}
           />
         </div>
         <div className="grid">
@@ -53,7 +115,7 @@ function App() {
             type="tel"
             placeholder="请输入手机号"
             value={state.phone}
-            onChange={e => handle(e, 'phone')}
+            onChange={(e) => handle(e, 'phone')}
           />
         </div>
         <div className="grid">
@@ -63,9 +125,15 @@ function App() {
             type="tel"
             placeholder="请输入验证码"
             value={state.code}
-            onChange={e => handle(e, 'code')}
+            onChange={(e) => handle(e, 'code')}
           />
-          <button className="code-btn border-1px" onClick={getCode}>获取验证码</button>
+          {!timing ? (
+            <button className="code-btn border-1px" onClick={getCode}>
+              获取验证码
+            </button>
+          ) : (
+            <div className="count">{second}s</div>
+          )}
         </div>
         <button className="submit-btn" onClick={submit}>
           兑换咖啡券
--- src/pages/details/Details.js
+++ src/pages/details/Details.js
@@ -2,6 +2,8 @@ import './Details.css';
 import ok from '../../assets/images/ok.png'
 import qrcode from '../../assets/images/qrcode.png'
 function Details() {
+  const code = sessionStorage.getItem('code')
+  console.log(22, code)
   return (
     <div className="details">
       <div className="header">
@@ -11,20 +13,24 @@ function Details() {
         <div className="line"></div>
         <div className="code">
           <span className="code-name">兑换码</span>
-          <span className="code-val">2123213123213213</span>
+          <span className="code-val">{code}</span>
         </div>
       </div>
       <div className="article">
         <span className="title">兑换方式</span>
-        <span className="text">1. 复制咖啡券兑换码；</span>
-        <span className="text">2. 长按下方二维码进入“ARABICA”微信小程序，在卡券兑换页中填入兑换码完成兑换；</span>
+        <ol className="ol">
+          <li className="text">复制咖啡券兑换码；</li>
+          <li className="text">
+            长按下方二维码进入“ARABICA”微信小程序，在卡券兑换页中填入兑换码完成兑换；
+          </li>
+        </ol>
       </div>
       <div className="footer">
         <img className="qrcode" src={qrcode} alt="qrcode" />
         <div className="tip">扫我就可以直接跳转噢^ ^</div>
       </div>
     </div>
-  );
+  )
 }
 
 export default Details;
