0616f18d4656717809702689343d3b84f4f8c85d
suntgr <suntgr@163.com>
2021-09-09 21:45:56 +0800

    222

--- src/pages/details/Details.js
+++ src/pages/details/Details.js
@@ -19,10 +19,17 @@ function Details() {
         <span className="title">兑换方式</span>
         <ol className="ol">
           <li className="text">复制咖啡券兑换码；</li>
+          <li className="text">微信小程序搜索“ARABICA”；</li>
+          <li className="text">打开并授权登录；</li>
+          <li className="text">点击右下角“我的”，进入“卡券兑换”选项；</li>
+          <li className="text">在券码输入框中输入兑换码，点击确认完成兑换；</li>
           <li className="text">
-            长按下方二维码进入“ARABICA”微信小程序，在卡券兑换页中填入兑换码完成兑换；
+            在小程序下单时结算页勾选抵扣使用，支持到店自提/外送，不与其他优惠同享。
           </li>
         </ol>
+        <p className="sub-text">
+          * 截屏保存后用微信扫一扫太阳码也可以直接跳转噢^v^
+        </p>
       </div>
       <div className="footer">
         <img className="qrcode" src={qrcode} alt="qrcode" />
